0289cb5e8c1be6e0c7913545e150595b3fb717b2
윤명기 <theyoonicon@gmail.com>
2024-07-24 23:56:54 +0900

    todos 수정

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template, redirect, url_for, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
