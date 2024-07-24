1052a95787590b2f2a2cf7c99fb8d1e89f0f35d8
윤명기 <theyoonicon@gmail.com>
2024-07-25 00:46:46 +0900

    123

--- app.py
+++ app.py
@@ -71,24 +71,18 @@ def login():
         if request.method == 'POST':
             data = request.get_json(silent=True)
             if data is None:
-                data = request.form
+                return jsonify({"message": "Invalid JSON data"}), 400
             username = data.get('username')
             password = data.get('password')
             user = User.query.filter_by(username=username).first()
             if user and bcrypt.check_password_hash(user.password, password):
                 access_token = create_access_token(identity=user.id)
-                if request.is_json:
-                    return jsonify({"message": "Login successful", "token": access_token}), 200
-                else:
-                    response = make_response(redirect(url_for('get_or_add_todos', username=username)))
-                    response.set_cookie('access_token', access_token, httponly=True)
-                    return response
+                return jsonify({"message": "Login successful", "token": access_token}), 200
             return jsonify({"message": "Invalid credentials"}), 401
         else:
             return render_template('login.html')
     except Exception as e:
         return jsonify({"message": "An error occurred", "error": str(e)}), 500
-
 
 def get_jwt_identity_from_request():
     auth_header = request.headers.get('Authorization')
